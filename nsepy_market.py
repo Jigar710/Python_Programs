e7a0dcdca4e7a8d98917a8cbcdf3879c4e34ca4f
Jigar710 <jigar.shekhat.777@gmail.com>
2021-01-26 07:22:01 +0530

    Update nsepy_market file

--- nsepy_market.py
+++ nsepy_market.py
@@ -3,7 +3,6 @@ import nsepy
 from datetime import datetime,timedelta,date
 import time 
 
-#help(nsepy.get_history)
 start = time.time()
 to_date = datetime.now()
 to_date = datetime.strftime(to_date,"%y,%m,%d")
@@ -14,5 +13,6 @@ df = nsepy.get_history(symbol = "RELIANCE",start= from_date,end=to_date)
 print(df)
 end = time.time()
 print(end-start)
+help(nsepy.get_history)
 
 
